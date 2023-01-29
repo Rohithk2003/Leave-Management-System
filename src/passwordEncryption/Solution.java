119c0b879eba26c46aa3024a966a13dd82eb3e41
Rohithk <67067796+Rohithk2003@users.noreply.github.com>
2023-01-29 20:38:46 +0530

    Update Solution.java

--- src/passwordEncryption/Solution.java
+++ src/passwordEncryption/Solution.java
@@ -3,21 +3,6 @@ package passwordEncryption;
 public class Solution {
     public static final String alpha = "abcdefghijklmnopqrstuvwxyz";
 
-    public static String decrypt(String cipherText, int shiftKey) {
-        cipherText = cipherText.toLowerCase();
-        String message = "";
-        for (int ii = 0; ii < cipherText.length(); ii++) {
-            int charPosition = alpha.indexOf(cipherText.charAt(ii));
-            int keyVal = (charPosition - shiftKey) % 26;
-            if (keyVal < 0) {
-                keyVal = alpha.length() + keyVal;
-            }
-            char replaceVal = alpha.charAt(keyVal);
-            message += replaceVal;
-        }
-        return message;
-    }
-
     public static String encrypt(String message, int shiftKey) {
         message = message.toLowerCase();
         String cipherText = "";
